19be64d8776e7aef6c9fa7f40b3f040cc250ebde
defcore22 <clchin2006@gmail.com>
2026-05-25 13:25:49 -0500

    output is incorrect

--- 2015/paper-2015-02/src/main.rs
+++ 2015/paper-2015-02/src/main.rs
@@ -6,60 +6,63 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-
 fn main() {
     // open the file
     let f = File::open("presents.txt").unwrap();
     // move the portion of the file into RAM
     let mut buf_reader = BufReader::new(f);
 
-    // declare string to appnd
+    // declare the string to append
     let mut line: String = String::new();
     
     // declare your prism
-    let (mut l,mut w,mut h)= (0,0,0);
+    // let (mut l,mut w,mut h)= (0,0,0);
 
     let mut total = 0;
     // read the first line
     while buf_reader.read_line(&mut line).unwrap() > 0 {
-        
+        total += slice_prism(&mut line);
+        line.clear();
     }
     println!("{}",total);
 
 }
 
-fn slice_prism(line:String) -> i32 {
+fn slice_prism(line: &mut String) -> u32 {
     
-    // declare the sides of prism
-    let mut prism = vec![0,0,0];
+    // declare the sides of prism [h,l,w]
+    let mut prism:Vec<u32> = vec![0,0,0];
     
     // declare the index we are in.
-    let mut index = 0;
+    let mut index:usize = 0;
 
-    // loop throuh the line and seperate l,w,h.
-    while index < 3 {
+    // declare decimal point
+    let mut point = 0;
 
-        // extract each sides
-        'Outer: for i in 0..line.len() {
-            // pop the last number and assign to num
-            let num = line.pop();
-            
-            // match the value.
-            match num {
-                None => break 'Outer,
-                Some('x') => continue,
-                _ => {
-                    let side:u32 = num.to_digit();
-                    prism[index] = num;
-                },
-            }
-
+    // extract each sides
+    for _i in 0..line.len() {
+        // pop the last number and assign to num
+        let num = line.pop();
+        
+        // match the value.
+        match num {
+            Some('x') => {
+                point = 0;
+                index += 1;
+                continue
+            },
+            _ => { 
+                // try without if let.
+                if let Some(side) = num.unwrap().to_digit(10) {
+                    prism[index] += side*10_u32.pow(point);
+                    point += 1;
+                }
+            },
         }
-        // increment index
-        index += 1;
+
     }
 
-    let mut present = 0;
-    present
+    // return 2*l*w + 2*w*h + 2*h*l
+    2*(prism[1]*prism[2] + prism[2]*prism[0] + prism[0]*prism[1])
 
 }
